abc50592aac1d13b29ad6b5f4a4e42bebb3d3af9
dimassdoubles <codedimas@gmail.com>
2022-04-07 21:40:44 +0700

    latihan-6 tugas-1 selesai

---
+++ latihan-6/tugas-1/TestPaket.java
@@ -0,0 +1,11 @@
+import kelasku.*;
+
+public class TestPaket {
+    public static void main(String[] args) {
+        Budi objekBudi = new Budi();
+        Joko objekJoko = new Joko();
+
+        objekBudi.info();
+        objekJoko.info();
+    }
+}
